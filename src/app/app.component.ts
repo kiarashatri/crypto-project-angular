1ea1ea06b27968b144dfa0e583cc4821aab27db8
kiarashatri <kiarashatri@gmail.com>
2023-03-02 17:43:02 +0330

    chore: dependencies installed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'crypto-project-angular';
+
+  onButtonClick(e: any) {
+    console.log(e.target.value);
+  }
 }
